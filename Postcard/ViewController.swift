ec4dd017b5067c89fd68efb9889ae1aab99c3de8
EJL1 <elyse.j.lefebvre@gmail.com>
2014-11-09 18:36:14 -0800

    Added nameLabel functionality

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
@@ -27,13 +28,21 @@ class ViewController: UIViewController {
 
     @IBAction func SendMailButtonPressed(sender: UIButton) {
         //Code will evaluate when we press the button
-        // Addign a comment here to test commits
+        // Adding a comment here to test commits
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
         
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
+        
         
         mailButton.setTitle("Message Sent", forState: UIControlState.Normal)
         
